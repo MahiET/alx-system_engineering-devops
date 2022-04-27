4ff380ff92859bc8edfad8b288eca8ec817d0c2e
MahiET <meazalemma2020@gmail.com>
2022-04-27 06:26:52 -0700

    recursive function that queries

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-from json import loads
-from requests import get
+import pprint
+import requests
+
+BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit. If no
     results are found for the given subreddit, the function should return None.
     """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    response = get(url, headers=headers, allow_redirects=False)
-    reddits = response.json()
-
-    try:
-        children = reddits.get('data').get('children')
-        for title in children:
-            hot_list.append(title.get('data').get('title'))
-        return hot_list
-    except:
-        print(None)
-        return 0
+     headers = {'User-agent': 'Unix:0-subs:v1'}
+    params = {'limit': 100}
+    if isinstance(after, str):
+        if after != "STOP":
+            params['after'] = after
+        else:
+            return hot_list
+    response = requests.get(BASE_URL.format(subreddit),
+                            headers=headers, params=params)
+    if response.status_code != 200:
+        return None
+    data = response.json().get('data', {})
+    after = data.get('after', 'STOP')
+    if not after:
+        after = "STOP"
+    hot_list = hot_list + [post.get('data', {}).get('title')
+                           for post in data.get('children', [])]
+    return recurse(subreddit, hot_list, after)
