12f4e5c38ebf163fc5131f1b6355e4e19d6b574d
MahiET <meazalemma2020@gmail.com>
2022-04-27 06:13:34 -0700

    If an invalid subreddit is given

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
+"""function that queries reddit api and returns number
+of subreddit."""
+import json
 import requests
-"""
-function that queries reddit api and returns number
-of subs
-"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    function definition of getting number of subs
-    """
-    url = "https://api.reddit.com/r/{}/about".format(subreddit)
-    header = {'User-Agent': 'CustomClient/1.0'}
-    req = requests.get(url, headers=header, allow_redirects=False)
-    if req.status_code != 200:
-        return (0)
-    req = req.json()
-    if "data" in req:
-        return (req.get("data").get("subscribers"))
+    user = {"User-Agent": "Meaza"}
+    request = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers=user)
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
     else:
-        return (0)
+        return 0
